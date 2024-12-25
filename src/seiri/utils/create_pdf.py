a0f6953055b2ec2579435d4f7866dcc6f5007858
darthtechworker <gaurav91pandey@gmail.com>
2024-12-24 16:27:26 -0800

    fixed alignment issues

--- src/seiri/utils/create_pdf.py
+++ src/seiri/utils/create_pdf.py
@@ -62,8 +62,13 @@ def insert_year_view(app, pdf_canvas):
             days_of_week = ["S", "M", "T", "W", "T", "F", "S"]
 
         months = calendar.month_name[1:]
+        month_font = "Helvetica"
         month_font_size = 35
-        padding_between_months = 20
+        padding_between_months = 30
+        week_font = "Helvetica-Bold"
+        day_of_week_font_size = 25
+        day_font = "Helvetica"
+        day_font_size = 25
 
         usable_width = app.device_width - app.margin_left - app.margin_right
         usable_height = app.device_height - app.margin_top - app.margin_bottom
@@ -72,37 +77,49 @@ def insert_year_view(app, pdf_canvas):
         month_height = (usable_height - 3 * padding_between_months) / 4
 
         vertical_spacing = month_height / 8
-        horizontal_spacing = month_width / 7
 
         x_offset = app.margin_left
-        y_offset = (
-            app.device_height - app.margin_top - month_font_size
-        )  # Adjust for the font size as reportlabs draws text from the bottom left corner
+        y_offset = app.device_height - app.margin_top - month_font_size
 
         for month_index, month_name in enumerate(months):
-            x = x_offset + (month_index % 3) * (month_width + padding_between_months)
-            y = y_offset - (month_index // 3) * (month_height + padding_between_months)
+            month_x = x_offset + (month_index % 3) * (
+                month_width + padding_between_months
+            )
+            month_y = y_offset - (month_index // 3) * (
+                month_height + padding_between_months
+            )
 
-            pdf_canvas.setFont("Helvetica", month_font_size)
-            pdf_canvas.drawString(x, y, month_name)
+            pdf_canvas.setFont(month_font, month_font_size)
+            text_width = pdf_canvas.stringWidth(month_name, month_font, month_font_size)
+            center_x = month_x + (month_width / 2)
+            pdf_canvas.drawString(center_x - text_width / 2, month_y, month_name)
 
-            day_x = x
-            day_y = y - vertical_spacing
+            day_of_week_y = month_y - vertical_spacing
 
-            pdf_canvas.setFont("Helvetica", 25)
-            for day in days_of_week:
-                pdf_canvas.drawString(day_x, day_y, day)
-                day_x += horizontal_spacing
+            pdf_canvas.setFont(week_font, day_of_week_font_size)
+            for i, day in enumerate(days_of_week):
+                day_center_x = month_x + (i * (month_width / 7)) + (month_width / 14)
+                text_width = pdf_canvas.stringWidth(
+                    day, week_font, day_of_week_font_size
+                )
+                pdf_canvas.drawString(day_center_x - text_width / 2, day_of_week_y, day)
 
-            day_y -= vertical_spacing
+            day_y = day_of_week_y - vertical_spacing
 
+            pdf_canvas.setFont(day_font, day_font_size)
             days = cal.monthdayscalendar(year, month_index + 1)
             for week in days:
-                day_x = x
-                for day in week:
+                for i, day in enumerate(week):
                     if day != 0:
-                        pdf_canvas.drawString(day_x, day_y, str(day))
-                    day_x += horizontal_spacing
+                        day_center_x = (
+                            month_x + (i * (month_width / 7)) + (month_width / 14)
+                        )
+                        text_width = pdf_canvas.stringWidth(
+                            str(day), day_font, day_font_size
+                        )
+                        pdf_canvas.drawString(
+                            day_center_x - text_width / 2, day_y, str(day)
+                        )
                 day_y -= vertical_spacing
 
         pdf_canvas.showPage()
