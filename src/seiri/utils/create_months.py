fee56986576759b7970ceb54a3e6bad8f59022c9
darthtechworker <gaurav91pandey@gmail.com>
2024-12-25 20:13:16 -0800

    added week numbers

--- src/seiri/utils/create_months.py
+++ src/seiri/utils/create_months.py
@@ -23,8 +23,11 @@ def insert_months(app, pdf_canvas):
 
         breadcrumb_font = "Helvetica-Bold"
         breadcrumb_font_size = 40 * font_scale_factor
-        week_font = "Helvetica"
+        day_of_week_font = "Helvetica"
         day_of_week_font_size = 30 * font_scale_factor
+        week_number_font = "Helvetica"
+        week_number_font_size = 27.5 * font_scale_factor
+        padding_for_week_number_labels = 2 * week_number_font_size
         day_font = "Helvetica"
         day_font_size = 30 * font_scale_factor
 
@@ -35,7 +38,7 @@ def insert_months(app, pdf_canvas):
             usable_width = app.device_height - app.margin_top - app.margin_bottom
             usable_height = app.device_width - app.margin_left - app.margin_right
 
-        day_width = usable_width / 7
+        day_width = (usable_width - padding_for_week_number_labels) / 7
         day_height = usable_height / 7
 
         vertical_spacing = usable_height / 7
@@ -84,22 +87,45 @@ def insert_months(app, pdf_canvas):
 
             day_of_week_y = y_offset - (vertical_spacing / 4)
 
-            pdf_canvas.setFont(week_font, day_of_week_font_size)
-            for i, day in enumerate(days_of_week):
-                day_center_x = x_offset + (i * day_width) + (day_width / 2)
+            pdf_canvas.setFont(day_of_week_font, day_of_week_font_size)
+            for day_index, day in enumerate(days_of_week):
+                day_center_x = (
+                    x_offset
+                    + padding_for_week_number_labels
+                    + (day_index * day_width)
+                    + (day_width / 2)
+                )
                 text_width = pdf_canvas.stringWidth(
-                    day, week_font, day_of_week_font_size
+                    day, day_of_week_font, day_of_week_font_size
                 )
                 pdf_canvas.drawString(day_center_x - text_width / 2, day_of_week_y, day)
 
             day_y = day_of_week_y - (vertical_spacing / 4)
 
-            pdf_canvas.setFont(day_font, day_font_size)
             days = cal.monthdayscalendar(app.year, month)
-            for week in days:
-                for i, day in enumerate(week):
+            for week_index, week in enumerate(days):
+                pdf_canvas.setFont(week_number_font, week_number_font_size)
+                week_label_x = x_offset + (padding_for_week_number_labels / 2)
+                week_label = f"Week {week_index + 1}"
+                week_label_height = pdf_canvas.stringWidth(
+                    week_label, week_number_font, week_number_font_size
+                )
+                week_label_y = day_y - (day_height / 2) - (week_label_height / 2)
+
+                pdf_canvas.saveState()
+                pdf_canvas.translate(week_label_x, week_label_y)
+                pdf_canvas.rotate(90)
+                pdf_canvas.drawString(0, 0, week_label)
+                pdf_canvas.restoreState()
+
+                pdf_canvas.setFont(day_font, day_font_size)
+                for day_index, day in enumerate(week):
                     if day != 0:
-                        day_center_x = x_offset + (i * day_width)
+                        day_center_x = (
+                            x_offset
+                            + padding_for_week_number_labels
+                            + (day_index * day_width)
+                        )
                         pdf_canvas.drawString(
                             day_center_x + day_font_size / 4,
                             day_y - day_font_size,
